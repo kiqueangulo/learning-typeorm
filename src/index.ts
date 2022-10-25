55fbb0143cfb303e1c662c2ee796898ce5f16a2a
kiqueangulo <kiqueangulo@gmail.com>
2022-10-25 02:41:36 -0400

    Define workflow and folder structure

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,9 @@
 import express from "express"
 
+import TaskRoutes from "./tasks/task.routes"
+
 const app = express()
+
+app.use("/api", TaskRoutes)
 
 app.listen(3030, () => console.log("App running on port: 3030"))
