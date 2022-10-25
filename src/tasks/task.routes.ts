71a79f39e2d8f98c98ee743d9512bd293828665e
kiqueangulo <kiqueangulo@gmail.com>
2022-10-25 03:00:22 -0400

    Set up routes

--- src/tasks/task.routes.ts
+++ src/tasks/task.routes.ts
@@ -6,6 +6,14 @@ const router = Router()
 
 const controller = new TaskController()
 
+router.post("/tasks", controller.create)
+
 router.get("/tasks", controller.list)
+
+router.get("/tasks/:taskId", controller.get)
+
+router.put("/tasks/:taskId", controller.update)
+
+router.delete("/tasks/:taskId", controller.remove)
 
 export default router
