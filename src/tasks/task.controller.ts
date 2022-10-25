20ea9dfc3da46bf42cf1cde0877fb3ae8e09dcf0
kiqueangulo <kiqueangulo@gmail.com>
2022-10-25 02:50:49 -0400

    Create controller as class

--- src/tasks/task.controller.ts
+++ src/tasks/task.controller.ts
@@ -1,1 +1,7 @@
-export class TaskController {}
+import { Request, Response, NextFunction } from "express"
+
+export class TaskController {
+  async list(req: Request, res: Response, next: NextFunction): Promise<void> {
+    res.status(200).json({ message: "Tasks from controller" })
+  }
+}
--- src/tasks/task.routes.ts
+++ src/tasks/task.routes.ts
@@ -1,5 +1,11 @@
 import { Router } from "express"
 
+import { TaskController } from "./task.controller"
+
 const router = Router()
+
+const controller = new TaskController()
+
+router.get("/tasks", controller.list)
 
 export default router
