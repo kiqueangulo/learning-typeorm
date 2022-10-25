6453b97fb6ef1fd6798401d7e8c9c57bb655c0a2
kiqueangulo <kiqueangulo@gmail.com>
2022-10-25 16:25:21 -0400

    Attemp to make handler for create but have a bug
    The key word this in the controller class shows undefined

--- src/tasks/task.controller.ts
+++ src/tasks/task.controller.ts
@@ -6,8 +6,12 @@ import { Task } from "../entity/Task"
 export class TaskController {
   constructor(private repository: DatabaseRepository<Task>) {}
 
-  async create(req: Request, res: Response, next: NextFunction): Promise<void> {
-    res.status(200).json({ message: "Create task" })
+  async create(req: Request, res: Response): Promise<void> {
+    // const task = await this.repository.create(req.body)
+
+    console.log("this.repository = ", this) // Bug: this is being thrown undefined
+
+    res.status(200).json({})
   }
 
   async list(req: Request, res: Response, next: NextFunction): Promise<void> {
