6e9a1954b62580847bdd312b699caf125297dcac
kiqueangulo <kiqueangulo@gmail.com>
2022-10-25 16:04:29 -0400

    Implement the interface in repository

--- src/tasks/task.repository.ts
+++ src/tasks/task.repository.ts
@@ -1,1 +1,24 @@
-export class TaskRepository {}
+import { DatabaseRepository, Id, Query } from "../types"
+import { Task } from "../entity/Task"
+
+export class TaskRepository implements DatabaseRepository<Task> {
+  create(data: Partial<Task>, query?: Query | undefined): Promise<Task> {
+    throw new Error("Method not implemented.")
+  }
+
+  list(query?: Query | undefined): Promise<Task[]> {
+    throw new Error("Method not implemented.")
+  }
+
+  get(id: Id, query?: Query | undefined): Promise<Task> {
+    throw new Error("Method not implemented.")
+  }
+
+  update(id: Id, data: Task, query?: Query | undefined): Promise<Task> {
+    throw new Error("Method not implemented.")
+  }
+
+  remove(id: Id, query?: Query | undefined): Promise<Task> {
+    throw new Error("Method not implemented.")
+  }
+}
